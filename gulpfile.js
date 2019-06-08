bd087fdb612ceb589eb0754978476d73d6734ac3
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-06-08 21:44:15 +0700

    Finished Section 6 Gulp Timeout

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,3 @@
-const gulp = require('gulp')
-const watch = require('gulp-watch')
-const postcss = require('gulp-postcss')
-const autoprefixer = require('autoprefixer')
-const cssvars = require('postcss-simple-vars')
-const nested = require('postcss-nested')
-const cssImport = require('postcss-import')
+require('./gulp/tasks/styles')
+require('./gulp/tasks/watch')
 
-gulp.task('default', () => {
-    console.log('Hooray - you created a Gulp task.')
-})
-
-gulp.task('html', () => {
-    console.log('Imagine something useful being done to your HTML here.')
-})
-
-gulp.task('styles', () => {
-    return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-    .pipe(gulp.dest('./app/temp/styles'))
-})
-
-gulp.task('watch', () => {
-
-    watch('./app/index.html', () => {
-        gulp.start('html')
-    })
-
-    watch('./app/assets/styles/**/*.css', () => {
-        gulp.start('styles')
-    })
-
-})
