96dc44b9fab06d118df051f939b2f85c678e516e
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-06-08 18:52:10 +0700

    Finish Section 5 CSS Architecture

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const postcss = require('gulp-postcss')
 const autoprefixer = require('autoprefixer')
 const cssvars = require('postcss-simple-vars')
 const nested = require('postcss-nested')
+const cssImport = require('postcss-import')
 
 gulp.task('default', () => {
     console.log('Hooray - you created a Gulp task.')
@@ -15,7 +16,7 @@ gulp.task('html', () => {
 
 gulp.task('styles', () => {
     return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'))
 })
 
